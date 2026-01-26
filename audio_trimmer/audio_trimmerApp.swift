9b8e416f1572bd73bdfc61571b1414c440af6c25
Shan_Chen <tarasuzy00@gmail.com>
2026-01-26 22:22:01 +0800

    Initial Commit

---
+++ audio_trimmer/audio_trimmerApp.swift
@@ -0,0 +1,17 @@
+//
+//  audio_trimmerApp.swift
+//  audio_trimmer
+//
+//  Created by Shan OvO on 2026/1/26.
+//
+
+import SwiftUI
+
+@main
+struct audio_trimmerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
